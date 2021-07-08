0a08c4bc5cdad0e0831040793b2f3bae64988ffb
Calvin Hyacinth <46787710+chyacin@users.noreply.github.com>
2021-07-08 11:58:18 +0200

    re-did the hardSkills and langues Routes

--- routes/langues.js
+++ routes/langues.js
@@ -18,28 +18,60 @@ languesRoutes.get('/:candidate_id/langues', (req, res) => {
 });
 
 languesRoutes.post('/:candidate_id/langues', (req, res) => {
+  const candidateId = req.params.candidate_id;
   const candidateLangues = {
     number: req.body.number,
     langueName: req.body.langueName,
   };
-  const candidateId = req.params.candidate_id;
-  db.query('INSERT INTO candidate_langues(number, langueName, candidate_id) VALUES (?, ?, ?)',
-    [candidateLangues.number,
-      candidateLangues.langueName,
-      candidateId],
-    (err, results) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Error saving Candidate langue');
+  db.query('SELECT langueName FROM candidate_langues WHERE candidate_id = ? AND number = ?',
+    [candidateId, candidateLangues.number],
+    (selectErr, selectResults) => {
+      if (selectErr) {
+        res.status(500).send('Error selecting the candidate language');
       } else {
-        const updatedCandidateLangue = {
-          id: results.insertId,
-          candidateId,
-          number: candidateLangues.number,
-          langueName: candidateLangues.langueName,
-        };
-        res.status(201).send(updatedCandidateLangue);
+        const langueFromDB = selectResults[0];
+        if (langueFromDB) {
+          const langueToUpdate = req.body;
+          db.query('UPDATE candidate_langues SET langueName = ? WHERE candidate_id = ? AND number = ?', [candidateLangues.langueName, candidateId, candidateLangues.number], (updateErr) => {
+            if (updateErr) {
+              console.log(updateErr);
+              res.status(500).send('Error updating the candidate language');
+            } else {
+              const updated = { ...langueFromDB, ...langueToUpdate };
+              res.status(200).send(updated);
+            }
+          });
+        } else {
+          db.query('INSERT INTO candidate_langues(number, langueName, candidate_id) VALUES (?, ?, ?)',
+            [candidateLangues.number,
+              candidateLangues.langueName,
+              candidateId],
+            (err, insertResults) => {
+              if (err) {
+                console.log(err);
+                res.status(500).send('Error saving Candidate langue');
+              } else {
+                const updatedCandidateLangue = {
+                  id: insertResults.insertId,
+                  candidateId,
+                  number: candidateLangues.number,
+                  langueName: candidateLangues.langueName,
+                };
+                res.status(201).send(updatedCandidateLangue);
+              }
+            });
+        }
       }
     });
 });
+
+languesRoutes.delete('/:candidate_id/langues/:number', (req, res) => {
+  db.query('DELETE FROM candidate_langues WHERE candidate_id = ? AND number = ?', [req.params.candidate_id, req.params.number], (err, results) => {
+    if (err) {
+      res.status(500).send('Error deleting a candidate language');
+    } else if (results.affectedRows) res.status(200).send(' 🎉candidate language deleted');
+    else res.status(404).send('candidate language not found');
+  });
+});
+
 module.exports = languesRoutes;
